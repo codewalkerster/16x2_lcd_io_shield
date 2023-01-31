8b9714e1ca4b4c55607854087d66e81d6226c4e0
Chris <codewalker@hardkernel.com>
2023-01-31 11:18:41 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-gcc -o example-lcd example-lcd.c -lwiringPi -lwiringPiDev -lm -lpthread -lrt -lcrypt
-gcc -o lcd_time lcd_time.c -lwiringPi -lwiringPiDev -lm -lpthread -lrt -lcrypt
-gcc -o wiring_isr wiring_isr.c -lwiringPi -lwiringPiDev -lm -lpthread -lrt -lcrypt
+rm -rf ./example-lcd ./lcd_time ./wiring_isr
+gcc -o example-lcd example-lcd.c -lwiringPi -lwiringPiDev -lm -lpthread -lrt -lcrypt $(pkg-config --cflags --libs libwiringpi2)
+gcc -o lcd_time lcd_time.c -lwiringPi -lwiringPiDev -lm -lpthread -lrt -lcrypt $(pkg-config --cflags --libs libwiringpi2)
+gcc -o wiring_isr wiring_isr.c -lwiringPi -lwiringPiDev -lm -lpthread -lrt -lcrypt $(pkg-config --cflags --libs libwiringpi2)

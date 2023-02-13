95e932b035a85b89b75a27379e2eb9843756a191
Chris <codewalker@hardkernel.com>
2023-02-13 11:14:18 +0900

    Add one more pin in ISR for example.
    16x2_IO_Shield has two switches,
    so I modify some codes to enable two switches.

--- wiring_isr.c
+++ wiring_isr.c
@@ -1,28 +1,37 @@
-#include <stdio.h>                                                              
-#include <wiringPi.h>                                                           
- 
-int volatile btnCounter;                                                        
- 
-void myInterrupt(void)                                                          
-{                                                                               
-        btnCounter++;                                                           
-}                                                                               
- 
-int main(void)                                                                  
-{                                                                               
-        if (wiringPiSetup() < 0) {                                              
-                printf("wiringPiSetup error.\n");                               
-                return 1;                                                       
-        }                                                                       
-        if (wiringPiISR(5, INT_EDGE_FALLING, &myInterrupt) < 0) {               
-                printf("wiringPiISR error.\n");                                 
-                return 1;                                                       
-        }                                                                       
- 
-        while(1) {                                                              
-                printf("%d\n", btnCounter);                                     
-                delay(1000);                                                    
-        }                                                                       
- 
-        return 0;                                                               
+#include <stdio.h>
+#include <wiringPi.h>
+
+int volatile btnCounter;
+
+void addInterrupt()
+{
+    btnCounter++;
+}
+
+void subInterrupt()
+{
+    btnCounter--;
+}
+
+int main()
+{
+    if (wiringPiSetup() < 0) {
+        printf("wiringPiSetup error.\n");
+        return 1;
+    }
+    if (wiringPiISR(5, INT_EDGE_FALLING, &addInterrupt) < 0) {
+        printf("wiringPiISR error.\n");
+        return 1;
+    }
+    if (wiringPiISR(6, INT_EDGE_FALLING, &subInterrupt) < 0) {
+        printf("wiringPiISR error.\n");
+        return 1;
+    }
+
+    while(true) {
+        printf("%d\n", btnCounter);
+        delay(1000);
+    }
+
+    return 0;
 }
